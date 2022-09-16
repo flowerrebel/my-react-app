57ff77ab2db8703ec17189e31d2fd95e821d4993
flowerrebel <98097006+flowerrebel@users.noreply.github.com>
2022-09-17 00:56:33 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/Header";
+import Action from "./components/Action";
+import Options from "./components/Options";
+import AddOption from "./components/Addoptions";
+
+class App extends React.Component {
+  render() {
+    const title = "my react app";
+    const subtitle = "what to emphasise on";
+    const options = [
+      "visit paris",
+      "visit egypt",
+      "visit tunis",
+      "visit kenya",
+    ];
+
+    return <div>
+      <Header titleData={title} subtitleData={subtitle} />
+      <Action />
+      <Options optionsData={options} />
+      <AddOption />
+       </div>;
+  }
+}
+
+export default App;
